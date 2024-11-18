a12877224f7361d982a2951e687dfd02d702cf57
Дюков Иван Павлович <i.dukov@nicetu.spb.ru>
2024-11-19 01:31:12 +0300

    base try-catch-finally

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,26 @@
 package com.example;
 
-public class App 
-{
-  public static void main( String[] args )
-  {
-    System.out.println( "Hello World!" );
+public class App {
+  public static void main( String[] args ) {
+    int res;
+    try {
+      res = 7 / 0;
+      System.out.println(res);
+    } catch (ArithmeticException e) {
+      System.out.println("ArithmeticException");
+    } finally {
+      System.out.println("Finally Arithmetic");
+    }
+
+    int[] arr = new int[3];
+    try {
+      for (int i = 0; i < 5; i++) {
+        arr[i] = arr[i] * 2;
+      }
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("ArrayIndexOutOfBoundsException");
+    } finally {
+      System.out.println("Finally ArrayIndex");
+    }
   }
 }
